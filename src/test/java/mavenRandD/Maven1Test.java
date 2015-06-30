691a93fd929372eb206104279030ec66a9f600aa
Abhishek Mishra <sabhishekmishra@gmail.com>
2015-06-30 17:36:29 +0530

    New branch

---
+++ src/test/java/mavenRandD/Maven1Test.java
@@ -0,0 +1,11 @@
+package mavenRandD;
+
+import org.testng.annotations.Test;
+import org.testng.log4testng.Logger;
+
+public class Maven1Test {
+@Test
+public void printPassedValue(){
+	System.out.println("---------------------Hello-------------");
+	}
+}
